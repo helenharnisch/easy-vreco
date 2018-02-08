beb749efe66df98b6b47110f95c73748d082dc53
helenharnisch <harnischvioleta@gmail.com>
2018-02-08 14:07:13 -0300

    Subiendo trabajo easy-vreco

---
+++ app.js
@@ -0,0 +1,67 @@
+
+function initMap(){
+  var map = new google.maps.Map(document.getElementById('map'),{
+    zoom: 5,
+    center: {lat: -9.1191427, lng: -77.0349046},
+    mapTypeControl: false,
+    zoomControl: false,
+    streetViewControl: false
+  });
+
+  function buscar(){
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+    }
+  }
+
+  document.getElementById('encuentrame').addEventListener('click', buscar);
+  var latitud, longitud;
+
+  var funcionExito = function(posicion){
+    latitud = posicion.coords.latitude;
+    longitud = posicion.coords.longitude;
+
+    var miUbicacion = new google.maps.Marker({
+      position: {lat:latitud, lng:longitud},
+      animation: google.maps.Animation.DROP,
+      map: map
+    });
+
+    map.setZoom(17);
+    map.setCenter({lat:latitud, lng:longitud});
+  }
+
+  var funcionError = function(error){
+    alert('tenemos un problema con encontrar tu ubicación');
+  }
+
+
+var inputPartida = document.getElementById('punto-partida');
+var inputDestino = document.getElementById('punto-destino');
+
+new google.maps.places.Autocomplete(inputPartida);
+new google.maps.places.Autocomplete(inputDestino);
+
+var directionsService = new google.maps.DirectionsService;
+var directionsDisplay = new google.maps.DirectionsRenderer;
+
+var calculateAndDisplayRoute = function(directionsService, directionsDisplay){
+  directionsService.route({
+    origin: inputPartida.value,
+    destination: inputDestino.value,
+    travelMode: google.maps.TravelMode.DRIVING
+  },
+  function(response, status){
+    if(status === 'OK'){
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('No encontramos una ruta');
+    }
+  });
+}
+directionsDisplay.setMap(map);
+var trazarRuta = function() {
+  calculateAndDisplayRoute(directionsService, directionsDisplay);
+};
+document.getElementById('trazar-ruta').addEventListener('click', trazarRuta);
+}
